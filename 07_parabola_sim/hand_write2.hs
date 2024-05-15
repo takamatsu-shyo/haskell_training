855523eee02d32b2e8cbad4824f32a148e0dbd22
Sho Yano <shoyano372@gmail.com>
2024-05-15 23:37:22 +0300

    input validation

--- 07_parabola_sim/hand_write2.hs
+++ 07_parabola_sim/hand_write2.hs
@@ -1,6 +1,8 @@
 import Text.Printf
 import Data.List
 import Data.Char (intToDigit)
+import Data.Maybe
+import Text.Read (readMaybe)
 
 -- Constants
 g :: Float
@@ -8,26 +10,39 @@ g = 9.81 -- Gravity
 v0 :: Float
 v0 = 200 -- Initial velocity (m/s)
 
+-- Input validation
+getValidInput :: String -> IO Float
+getValidInput prompt = do
+    putStrLn prompt
+    input <- getLine
+    case readMaybe input of
+        Just value ->
+            if value < 0
+            then do
+                putStrLn "Negative value is not acceptable. Set it to zero"
+                return 0
+            else return value
+
 -- Calculating the projectile's position
-position :: Float -> Float -> (Float, Float)
-position theta t = (x, y)
+position :: Float -> Float -> Float -> (Float, Float)
+position theta t y0 = (x, y)
     where 
         vx = cos (theta * pi / 180) :: Float
         vy = sin (theta * pi / 180) :: Float
         x = v0 * vx * t
-        y = v0 * vy * t - 0.5 * g * t^2
+        y = v0 * vy * t - 0.5 * g * t^2 + y0
 
 -- Generate trajectory points
-trajectory :: Float -> [(Float, Float)]
-trajectory theta = takeWhile ((>=0) . snd) [position theta t | t <- [0,0.01..]]
+trajectory :: Float -> Float -> [(Float, Float)]
+trajectory theta y0 = takeWhile ((>=0) . snd) [position theta t y0 | t <- [0,0.01..]]
 
 -- ASCII plot
-plotTrajectory :: [(Float, Float)] -> IO ()
-plotTrajectory points = do
+plotTrajectory :: [(Float, Float)] -> Float -> IO ()
+plotTrajectory points y0 = do
     let maxY = maximum $ map snd points
         maxX = maximum $ map fst points
-        ratioX = 4000 / fromIntegral (width - 1)
-        ratioY = 2300 / fromIntegral (height - 1)
+        ratioX = 6000 / fromIntegral (width - 1)
+        ratioY = 3500 / fromIntegral (height - 1)
 
         points' = [(floor (x / ratioX), floor(y / ratioY)) | (x,y) <- points]
         grid = replicate height (replicate width ' ')
@@ -56,10 +71,14 @@ height = 20
 
 main :: IO()
 main = do
-    putStrLn "Enter launch angle (deg):"
-    theta <- readLn
-    let traj = trajectory theta
-    plotTrajectory traj
+    --putStrLn "Enter launch angle (deg):"
+    theta <- getValidInput "Enter launch angle (deg):"
+    putStrLn "Enter launch hight (meter):"
+    y0 <- readLn 
+
+    let traj = trajectory theta y0
+
+    plotTrajectory traj y0
     let (lastX, _) = last traj
     printf "Flying distance: %.2f meters\n" lastX
 
